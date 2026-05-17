3832c57fa20078e695bae06da90dcd00a2cd1a91
Done <dragomir@nextblink.com>
2026-05-17 20:22:41 +0200

    fix: add migration to rename Functions.OrgUnitType column to CommitteeType

---
+++ src/backend/Marsipan.Membership.Middleware/Migrations/20260517182204_RenameFunctionOrgUnitTypeToCommitteeType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marsipan.Membership.Middleware.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameFunctionOrgUnitTypeToCommitteeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrgUnitType",
+                table: "Functions",
+                newName: "CommitteeType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CommitteeType",
+                table: "Functions",
+                newName: "OrgUnitType");
+        }
+    }
+}
